95473823df5491c1be6fde4b9f37d57381223e18
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2020-05-05 16:36:58 +0900

    Create S3 bucket and manage tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket  = "kazuy-circleci-ecs-sample-terraform"
+    region  = "ap-northeast-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "kazuy-circleci-ecs-sample-terraform"
+  versioning {
+    enabled = true
+  }
 }
